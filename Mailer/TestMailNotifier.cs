5ba8c18d26d89fca5159a215d83972831f7d231a
oakio <ilya.dubadenko@gmail.com>
2012-12-31 00:35:04 +0300

    Added test implementation of email notifier

---
+++ Mailer/TestMailNotifier.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Concurrent;
+using NLog;
+
+namespace Codestellation.Mailer
+{
+    public class TestMailNotifier : IMailNotifier
+    {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+        public static TestMailNotifier Global = new TestMailNotifier();
+
+        public ConcurrentQueue<object> Outgoing { get; private set; }
+
+        public TestMailNotifier()
+        {
+            Outgoing = new ConcurrentQueue<object>();
+        }
+
+        public void Send(object mail)
+        {
+            Outgoing.Enqueue(mail);
+            Log.Info("Email '{0}' was enqueued to outgoing queue", mail);
+        }
+    }
+}
