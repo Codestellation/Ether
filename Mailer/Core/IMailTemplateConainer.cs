a4c7482ba8c4e8b6612c44538ee7c21276c4fc42
oakio <ilya.dubadenko@gmail.com>
2012-12-31 13:29:25 +0300

    Added MailTemplateConainer abstraction

---
+++ Mailer/Core/IMailTemplateConainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Codestellation.Mailer.Core
+{
+    public interface IMailTemplateConainer
+    {
+        IMailTemplate Get(Type mailType);
+    }
+}
