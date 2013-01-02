adb74563fa8571997dc9c236579faa2056c20ef5
oakio <ilya.dubadenko@gmail.com>
2013-01-02 21:22:13 +0300

    Added IsTypeInHierarchy method

---
+++ Mailer.Tests/Core/MailingListBrokerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Codestellation.Mailer.Core;
+using NUnit.Framework;
+
+namespace Codestellation.Mailer.Tests.Core
+{
+    [TestFixture]
+    public class MailingListBrokerTests
+    {
+        [Test]
+        [TestCase("*")]
+        [TestCase("Codestellation")]
+        [TestCase("Codestellation.*")]
+        [TestCase("Codestellation.Mailer")]
+        [TestCase("Codestellation.Mailer.*")]
+        [TestCase("Codestellation.Mailer.MailNotifier")]
+        [TestCase("Codestellation.Mailer.MailNotifier.*")]
+        public void IsTypeInHierarchy_should_return_true_if_type_included_in_hierarchy(string hierarchy)
+        {
+            Assert.That(MailingListBroker.IsTypeInHierarchy(typeof (MailNotifier), hierarchy), Is.True);
+        }
+
+        [Test]
+        [TestCase("System")]
+        [TestCase("System.*")]
+        [TestCase("Codestellation.Mail")]
+        [TestCase("Codestellation.Mail.*")]
+        [TestCase("Codestellation.Mailer.Mail")]
+        [TestCase("Codestellation.Mailer.Mail.*")]
+        [TestCase("Codestellation.Mailer.MailNotifier.Core")]
+        [TestCase("Codestellation.Mailer.MailNotifier.Core.*")]
+        public void IsTypeInHierarchy_should_return_false_if_type_not_included_in_hierarchy(string hierarchy)
+        {
+            Assert.That(MailingListBroker.IsTypeInHierarchy(typeof(MailNotifier), hierarchy), Is.False);
+        }
+    }
+}
---
+++ Mailer/Core/MailingListBroker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Codestellation.Mailer.Core
+{
+    public class MailingListBroker : IMailingListBroker
+    {
+        public string[] GetRecepients(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool IsTypeInHierarchy(Type type, string hierarchy)
+        {
+            if (String.Equals(hierarchy, "*")) // any type
+            {
+                return true;
+            }
+
+            string[] hierarchyTokens = hierarchy.Split('.');
+            string[] typeNameTokens = type.FullName.Split('.');
+            int hierarchyTokensCount = hierarchyTokens.Length;
+            int typeNameTokensCount = typeNameTokens.Length;
+
+            for (int i = 0; i < Math.Min(hierarchyTokensCount, typeNameTokensCount); i++)
+            {
+                string hierarchyToken = hierarchyTokens[i];
+
+                if (String.Equals(hierarchyToken, "*"))
+                {
+                    return true;
+                }
+                if (String.Equals(hierarchyToken, typeNameTokens[i], StringComparison.CurrentCultureIgnoreCase) == false)
+                {
+                    return false;
+                }
+            }
+
+            if (hierarchyTokensCount > typeNameTokensCount && String.Equals(hierarchyTokens[typeNameTokensCount], "*") == false)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
